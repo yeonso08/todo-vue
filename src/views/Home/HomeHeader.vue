0cba78ce590e4750effb67ab91a7bf7c3109886b
yeonso08 <hjng0825@gmail.com>
2023-09-08 16:35:47 +0900

    feat : 헤더 추가

---
+++ src/views/Home/HomeHeader.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>
+            Todo it!
+        </h1>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.h3 {
+    color: aquamarine;
+}
+</style>
