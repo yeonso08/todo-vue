1eb6a0a2aadecff2801ae6cd277dac1aeeb8642d
yeonso08 <hjng0825@gmail.com>
2023-09-08 15:26:28 +0900

    fear : 라우터 연결

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,11 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-link to="/">Home</router-link>
+    <div> <router-link to="/login">Login</router-link>
+    </div>
+    <router-view></router-view>
   </div>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
 <style>
 #app {
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,22 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import Home from "../views/Home/Home.vue";
+import Login from "../views/Login/Login.vue";
 
-// 플러그인을 실행하기 위해서 필요한 코드
 Vue.use(VueRouter);
 
-// VueRouter로 인스턴스를 생성하고 export default 로 꺼냄.
 export default new VueRouter({
-  // routes: Vue Router에 의해서 컨트롤되는 페이지의 정보를 담는 것
-  routes: [],
+  mode: "history",
+  routes: [
+    {
+      path: `/`,
+      name: "home",
+      component: Home,
+    },
+    {
+      path: `/login`,
+      name: "login",
+      component: Login,
+    },
+  ],
 });
