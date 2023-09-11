67c183d5b55926c2de04392d39c711c31086ff47
yeonso08 <hjng0825@gmail.com>
2023-09-11 17:47:01 +0900

    feat : getToDos api 추가

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,13 @@
 import axios from "axios";
 
-export default function setToDos(payload) {
-  const url = "https://dummyjson.com/todos/add";
+function setToDos(payload) {
+  const url = "https://dummyjson.com/all/add";
   return axios.post(url, payload);
 }
+
+function getToDos() {
+  const url = "https://dummyjson.com/todos";
+  return axios.get(url);
+}
+
+export default { setToDos, getToDos };
