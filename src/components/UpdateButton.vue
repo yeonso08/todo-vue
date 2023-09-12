44c64bea2052e9f0f67e3a0ca8a8cefb7281b933
yeonso08 <hjng0825@gmail.com>
2023-09-12 11:20:08 +0900

    feat : updatebutton 생성 및 구현

---
+++ src/components/UpdateButton.vue
@@ -0,0 +1,25 @@
+<template>
+    <button @click="complettodo">V</button>
+</template>
+  
+<script>
+export default {
+    props: ['id'],
+    methods: {
+        complettodo() {
+            this.$emit('updateTodo', this.id);
+        }
+    }
+}
+</script>
+
+<style scoped>
+button {
+    border: none;
+    background-color: white;
+    color: greenyellow;
+    font-weight: 800;
+    margin-right: 10px;
+}
+</style>
+  
