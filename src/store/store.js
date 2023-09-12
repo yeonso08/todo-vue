f2f3220e6178f2dac7536649e83b75a77eaf7457
yeonso08 <hjng0825@gmail.com>
2023-09-12 11:19:37 +0900

    feat : updatet action 추가

--- src/store/store.js
+++ src/store/store.js
@@ -6,11 +6,10 @@ Vue.use(Vuex);
 export const store = new Vuex.Store({
   state: {
     toDos: [
-      { id: 0, todo: "leave" },
-      { id: 1, todo: "Go Home" },
-      { id: 2, todo: "Take a rest at home" },
+      { id: 0, todo: "leave", isCompleted: false },
+      { id: 1, todo: "Go Home", isCompleted: false },
+      { id: 2, todo: "Take a rest at home", isCompleted: false },
     ],
-    newTodoContent: "",
   },
   mutations: {
     ADD_TODO(state, payload) {
@@ -21,6 +20,12 @@ export const store = new Vuex.Store({
     DELETE_TODO(state, id) {
       state.toDos = state.toDos.filter(toDo => toDo.id !== id);
     },
+    COMPLET_TODO(state, id) {
+      const todo = state.toDos.find(todo => todo.id === id);
+      if (todo) {
+        todo.isCompleted = !todo.isCompleted;
+      }
+    },
   },
   actions: {
     ADD_TODO({ commit }, payload) {
@@ -28,6 +33,9 @@ export const store = new Vuex.Store({
     },
     DELETE_TODO({ commit }, id) {
       commit("DELETE_TODO", id);
+    },
+    COMPLET_TODO({ commit }, id) {
+      commit("COMPLET_TODO", id);
     },
   },
 });
